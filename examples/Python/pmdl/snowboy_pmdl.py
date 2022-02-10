bb7a7800ebac159263a232cbf6baa2c4888abf15
Keshav Nemali <keshav.nemali@maistering.com>
2022-02-10 17:47:50 +0530

    Changed folder name in dockerfile again

--- examples/Python/pmdl/snowboy_pmdl.py
+++ examples/Python/pmdl/snowboy_pmdl.py
@@ -4,9 +4,9 @@ import platform
 
 if platform.system() == "Darwin":
     sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), "lib/osx/pmdl"))
-    from snowboy-master import *
+    from snowboy_master import *
 elif platform.linux_distribution()[0] == "Ubuntu" and platform.linux_distribution()[1] == "16.04":
     sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), "lib/ubuntu64/pmdl"))
-    from snowboy-master import *
+    from snowboy_master import *
 else:
     raise ImportError("pmdl generator only runs on OSX or Ubuntu 16.04.")
